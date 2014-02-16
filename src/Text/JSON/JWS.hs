7464db514586b84d69a7a819135f9bcd7747cbe0
Mark Roberts <markandrusroberts@gmail.com>
2014-02-15 18:18:05 -0800

    Fleshing out JWS; updated functions

--- src/Text/JSON/JWS.hs
+++ src/Text/JSON/JWS.hs
@@ -2,13 +2,15 @@ module Text.JSON.JWS
   ( -- * JSON Web Signature (JWS)
     JWS
   , toByteString
-  , Key
-  , jws
+  , sign
     -- ** Header
   , Header(..)
+    -- *** Algorithm
   , Algorithm(..)
-    -- ** Payload
-  , Payload(..)
+    -- *** Type
+  , Type(..)
+  -- ** Payload
+  , Payload
   ) where
 
 import Crypto.Hash.SHA256
@@ -19,7 +21,9 @@ import qualified Data.ByteString.Base64.URL as B64
 import qualified Data.ByteString.Char8 as C
 import Data.ByteString.Lazy (toStrict)
 import Data.HashMap.Strict (fromList)
+import Data.Maybe (catMaybes)
 import qualified Data.Text as T
+import Network.URI
 
 {- "base64url" Encoding -}
 
@@ -40,32 +44,68 @@ encodeJSON = toBase64URL . toStrict . encode . toJSON
 
 {- JSON Web Signature (JWS) -}
 
+-- | A data structure representing a digitally signed or MACed message.
 newtype JWS = JWS { toByteString :: BS.ByteString }
-  deriving Eq
 
 instance Show JWS where
   show = show . toByteString
 
-type Key = BS.ByteString
-
-jws :: ToJSON a => Key -> Header -> Payload a -> JWS
-jws k h p =
+sign :: Header -> Payload -> JWS
+sign h p =
   let i = signingInput h p
-      s = toBase64URL . getSignature $ signature k i
+      s = toBase64URL . getSignature $ signature (alg h) i
   in  JWS $ getSigningInput i `C.append` ('.' `C.cons` getEncoding s)
 
 {- Header -}
 
-newtype Header = Header { alg :: Algorithm }
-  deriving (Show, Read, Eq)
+-- TODO(mroberts): Implement.
+type JWK = String
+
+-- | JSON object containing the parameters describing the cryptographic
+-- operations and parameters employed.
+data Header = Header
+    { alg  :: Algorithm       -- ^ Algorithm
+    , jku  :: Maybe URI       -- ^ 'JWK' Set URL
+    , jwk  :: Maybe JWK       -- ^ JSON Web Key
+    , kid  :: Maybe String    -- ^ Key ID
+    , x5u  :: Maybe URI       -- ^ X.509 URL
+    , x5c  :: Maybe [String]  -- ^ X.509 Certificate Chain
+    , x5t  :: Maybe String    -- ^ X.509 Certificate SHA-1 Thumbprint
+    , typ  :: Maybe Type      -- ^ Type
+    , cty  :: Maybe String    -- ^ Content Type
+    , crit :: Maybe [String]  -- ^ Critical
+    , unregistered :: [(String, Value)] }
+  deriving (Show, Eq)
 
 instance ToJSON Header where
-  toJSON (Header alg) = object
-    [ T.pack "typ" .= T.pack "JWT"
-    , T.pack "alg" .= alg ]
+  toJSON h = toJSON . fromList $ catMaybes
+    [ Just $ T.pack "alg" .= alg h
+    , fmap (\jku  -> T.pack "jku"  .= toJSON (show  jku)) $ jku  h
+    , fmap (\jwk  -> T.pack "jwk"  .= toJSON        jwk)  $ jwk  h
+    , fmap (\kid  -> T.pack "kid"  .= toJSON        kid)  $ kid  h
+    , fmap (\x5u  -> T.pack "x5u"  .= toJSON (show  x5u)) $ x5u  h
+    , fmap (\x5c  -> T.pack "x5c"  .= toJSON        x5c)  $ x5c  h
+    , fmap (\x5t  -> T.pack "x5t"  .= toJSON        x5t)  $ x5t  h
+    , fmap (\typ  -> T.pack "typ"  .= toJSON        typ)  $ typ  h
+    , fmap (\cty  -> T.pack "cty"  .= toJSON        cty)  $ cty  h
+    , fmap (\crit -> T.pack "crit" .= toJSON        crit) $ crit h
+    ] ++ (map (\(k, v) -> (T.pack k, v)) $ unregistered h)
 
-data Algorithm = HS256  -- ^ HMAC using SHA-256
-  deriving (Show, Read, Eq)
+data Type
+    = JWT
+  deriving (Show, Eq)
+
+instance ToJSON Type where
+  toJSON = toJSON . T.pack . show
+
+data Algorithm
+    = HS256 BS.ByteString  -- ^ HMAC using SHA-256
+    | None       -- ^ Plaintext
+  deriving Eq
+
+instance Show Algorithm where
+  show (HS256 _) = "HS256"
+  show None = "none"
 
 instance ToJSON Algorithm where
   toJSON = toJSON . T.pack . show
@@ -75,32 +115,34 @@ instance ToJSON Algorithm where
 
 {- Payload -}
 
-newtype Payload a = Payload { getPayload :: a }
-  deriving (Show, Read, Eq)
-
-instance ToJSON a => ToJSON (Payload a) where
-  toJSON = toJSON . getPayload
+-- | The sequence of octets to be secured — a.k.a., the message. The payload can
+-- contain an arbitrary sequence of octets.
+type Payload = BS.ByteString
 
 -- test2 = encodePayload . Payload . Object $ fromList
 --   [ ("iss", toJSON $ T.pack "joe")
 --   , ("exp", toJSON (1300819380 :: Integer))
 --   , ("http://example.com/is_root", toJSON True) ]
 
+-- | The input to the digital signature or MAC computation.
 newtype SigningInput = SigningInput { getSigningInput :: BS.ByteString }
   deriving Eq
 
 instance Show SigningInput where
   show = show . getSigningInput
 
-signingInput :: ToJSON a => Header -> Payload a -> SigningInput
+signingInput :: Header -> Payload -> SigningInput
 signingInput h p = SigningInput $ getEncoding (encodeJSON h) `C.append`
-    ('.' `C.cons` getEncoding (encodeJSON p))
+    ('.' `C.cons` getEncoding (toBase64URL p))
 
+-- | Digital signature or MAC over the 'JWS' 'Header' and the 'JWS' 'Payload'.
 newtype Signature = Signature { getSignature :: BS.ByteString }
   deriving Eq
 
 instance Show Signature where
   show = show . getSignature
 
-signature :: Key -> SigningInput -> Signature
-signature k = Signature . hmac hash (BS.length k) k . getSigningInput
+-- FIXME(mroberts): Not sure the second parameter of 'hash' is correct.
+signature :: Algorithm -> SigningInput -> Signature
+signature (HS256 key) = Signature . hmac hash (BS.length key) key . getSigningInput
+signature None = Signature . getSigningInput
--- src/Text/JSON/JWT.hs
+++ src/Text/JSON/JWT.hs
@@ -1,28 +1,60 @@
-{-#LANGUAGE OverloadedStrings #-}
-
 module Text.JSON.JWT (
   -- * JSON Web Token (JWT)
     JWT
   , toByteString
-  , Key
-  , jwt
-  -- ** Header
-  , Header
-  -- ** Payload
-  , Payload(..)
-  , Claim
-  , ClaimsSet
+  , toJWS
+  , sign
+  -- -- ** Header
+  -- , JWS.Header(..)
+  -- -- *** Algorithm
+  -- , JWS.Algorithm(..)
+  -- -- *** Type
+  -- , JWS.Type(..)
+  -- ** Claims Set
+  , ClaimsSet(..)
+  -- *** Claim
+  , Claim(..)
+  , IntDate
+  , StringOrURI
+  , stringOrURI
   ) where
 
 import Data.Aeson
 import qualified Data.ByteString as BS
+import Data.ByteString.Lazy (toStrict)
 import Data.HashMap.Strict (fromList)
+import Data.Maybe (catMaybes)
 import qualified Text.JSON.JWS as JWS
+import Network.URI
+
+{- IntDate -}
+
+-- | A JSON numeric value representing the number of seconds from
+-- 1970-01-01T0:0:0Z UTC until the specified UTC date/time.
+type IntDate = Integer
+
+{- StringOrURI -}
+
+-- | A JSON string value, with the additional requirement that while arbitrary
+-- string values MAY be used, any value containing a \":\" character MUST be a
+-- URI.
+newtype StringOrURI = StringOrURI { getStringOrURI :: Either String URI }
+  deriving (Show, Eq)
+
+instance ToJSON StringOrURI where
+  toJSON = either toJSON toJSON . fmap show . getStringOrURI
+
+-- | Parse a 'String' to a 'StringOrURI'.
+stringOrURI :: String -> Maybe StringOrURI
+stringOrURI string = if ':' `elem` string
+  then Just . StringOrURI $ Left string
+  else fmap (StringOrURI . Right) $ parseURI string
 
 {- JSON Web Token (JWT) -}
 
+-- | A string representing a set of claims as a JSON object that is encoded in a
+-- 'JWS.JWS', enabling the claims to be digitally signed or MACed.
 newtype JWT = JWT { toJWS :: JWS.JWS }
-  deriving Eq
 
 instance Show JWT where
   show = show . toJWS
@@ -30,10 +62,9 @@ instance Show JWT where
 toByteString :: JWT -> BS.ByteString
 toByteString = JWS.toByteString . toJWS
 
-type Key = BS.ByteString
-
-jwt :: Key -> Header -> Payload -> JWT
-jwt k h = JWT . JWS.jws k h . JWS.Payload
+-- | Create a 'JWT' by encoding a 'ClaimsSet' in a 'JWS.JWS'.
+sign :: JWS.Header -> ClaimsSet -> JWT
+sign h = JWT . JWS.sign h . toStrict . encode
 
 {- Header -}
 
@@ -41,16 +72,38 @@ type Header = JWS.Header
 
 {- Payload -}
 
-newtype Payload = Payload { getPayload :: ClaimsSet }
-  deriving (Show, Eq)
-
-instance ToJSON Payload where
-  toJSON = toJSON . fromList . getPayload
-
-{- Claim -}
-
-type Claim = (String, Value)
+type Payload = ClaimsSet
 
 {- Claims Set -}
 
-type ClaimsSet = [Claim]
+-- | A JSON object that contains the 'Claim's conveyed by the 'JWT'.
+newtype ClaimsSet = ClaimsSet { toList :: [Claim] }
+  deriving (Show, Eq)
+
+instance ToJSON ClaimsSet where
+  toJSON = toJSON . fromList . catMaybes . map f . toList where
+    f (Iss iss)      = Just ("iss", toJSON iss)
+    f (Sub sub)      = Just ("sub", toJSON sub)
+    f (Aud [])       = Nothing
+    f (Aud (aud:[])) = Just ("aud", toJSON aud)
+    f (Aud aud)      = Just ("aud", toJSON aud)
+    f (Exp exp)      = Just ("exp", toJSON exp)
+    f (Nbf nbf)      = Just ("nbf", toJSON nbf)
+    f (Iat iat)      = Just ("iat", toJSON iat)
+    f (Jti jti)      = Just ("jti", toJSON jti)
+    f (Unregistered "" _)      = Nothing
+    f (Unregistered key value) = Just (key, value)
+
+{- Claim -}
+
+-- | A piece of information asserted about a subject.
+data Claim
+  = Iss StringOrURI    -- ^ Issuer
+  | Sub StringOrURI    -- ^ Subject
+  | Aud [StringOrURI]  -- ^ Audience
+  | Exp IntDate        -- ^ Expiration Time
+  | Nbf IntDate        -- ^ Not Before
+  | Iat IntDate        -- ^ Issued At
+  | Jti String         -- ^ 'JWT' ID
+  | Unregistered String Value
+  deriving (Show, Eq)
